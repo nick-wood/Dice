3fe0ab8dba80c6ac315b625bcd6bae926829837e
Nick Wood <nickwood@Nicks-MacBook-Pro.local>
2015-07-22 16:40:24 -0700

    - Added rollDice() Method to Roll.swift. This method rolls numberOfDice amount of dice of dieSides sides, saves each individual roll to individualScores[], and keeps track of the total score. - Removed unnecessary interface stuff.

--- Dice/MainViewController.swift
+++ Dice/MainViewController.swift
@@ -10,15 +10,14 @@ import UIKit
 
 class MainViewController: UIViewController {
   
-  // These variables will be set by a two column picker on the main screen. They represent the number of dice the user is rolling  (numberOfDice) and the number of sides on each of those dice (dieSides). Rolls of mixed die type not supported in v1 of the app.
+  // These variables will be set by a two column picker on the main screen. They represent the number of dice the user is rolling  (numberOfDice) and the number of sides on each of those dice (dieSides). Rolls of mixed die type not supported in v1 of the app. Default value is 4d20, because best dice.
   
-  var numberOfDice: Int = 1
+  var numberOfDice: Int = 4
   var dieSides: Int = 20
+  var rollResults: [Roll] = []
   
   // This array holds the results of each roll. Entries are an object subclassed from the Roll class. Their specific properties can be accessed, including number of dice, die sides, and scores of individual die rolls within the Roll.
   
-  var rollResults = []
-
   override func viewDidLoad() {
     super.viewDidLoad()
   }
@@ -27,9 +26,9 @@ class MainViewController: UIViewController {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-  @IBAction func buttonPressed() {
-    println("Against all odds!")
+  
+  @IBAction func rollButtonPressed() {
+    println("You pressed the roll button. Rad.")
   }
 }
 
--- Dice/Roll.swift
+++ Dice/Roll.swift
@@ -14,10 +14,28 @@ class Roll {
   let dieSides: Int
   
   var individualScores = [Int]()
+  var totalRoll: Int = 0
   
   init(numberOfDice: Int, dieSides: Int) {
     
     self.numberOfDice = numberOfDice
     self.dieSides = dieSides
   }
+  
+  func rollDice(numberOfDice: Int, dieSides: UInt32) {
+    
+    var count = 1
+    
+    do {
+      
+      var result = Int(arc4random_uniform(dieSides))
+      
+      individualScores.append(result)
+      totalRoll += result
+      count++
+      
+    } while (count <= numberOfDice);
+  }
+
+  
 }
